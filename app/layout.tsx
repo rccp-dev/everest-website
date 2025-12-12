7e215a2674c9b6e5b60bd32af16890fa4e1ec4a8
rccp-dev <ricardocustodio0110@gmail.com>
2025-12-12 13:55:01 -0300

    Savatto

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Soluz",
-  description: "Soluz - Design & Software. Desenvolvendo sua marca junto com você!",
+  title: "Savatto",
+  description: "Savatto - Design & Software. Desenvolvendo sua marca junto com você!",
 };
 
 export default function RootLayout({
