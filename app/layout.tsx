1ce8fb458937e750103411cdcef2a25960f8a265
rccp-dev <ricardocustodio0110@gmail.com>
2025-12-12 21:35:35 -0300

    12/12/25

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`antialiased`}
+        className={`overflow-x-hidden antialiased`}
       >
         {children}
       </body>
